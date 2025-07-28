3d1faf43c3b50e0322bceec030c1cb7baeb98bb7
mskumargvd <santosh.modekurty@gmail.com>
2025-07-29 02:46:17 +0530

    V15

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Icon from './components/Icon';
+import Icon from './components/Icon'; // <-- CORRECT: Importing the Icon component
 
-const icons = {
+export const icons = {
   cyberSecurity: "M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z",
   cloud: "M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z",
   devops: "M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4",
@@ -13,8 +13,6 @@ const icons = {
   twitter: "M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.71v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84",
   linkedin: "M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z",
 };
-
-export { Icon, icons };
 
 export const courses = [
     { id: 'cyber-security', icon: <Icon path={icons.cyberSecurity} className="w-16 h-16 mx-auto"/>, title: 'Cyber Security', description: 'Currently available. Dive deep into ethical hacking, network defense, and threat intelligence.' },
