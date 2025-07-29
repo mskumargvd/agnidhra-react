0428fa440c5fcb58084f199972eb1e7bbd09b337
mskumargvd <santosh.modekurty@gmail.com>
2025-07-29 19:33:46 +0530

    v15

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
  //   import { Icon, icons } from '../data';
 
-import Icon from './components/Icon'; 
-import icons from './components/data'; 
+import Icon from './Icon';
+import icons from '../data';
 
 const Footer = ({ navigateTo }) => (
     <footer className="bg-black/50 text-white py-8">
